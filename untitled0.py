62ac153830a0d94de2b0dc2e3a58e6064f2bd60d
Gábor <79317065+type5sonata@users.noreply.github.com>
2022-09-05 22:34:48 +0200

    Add files via upload

---
+++ untitled0.py
@@ -0,0 +1,11 @@
+"""
+What sorts of things do we need to do?
+
+- Render map.
+- Add timeline.
+- Add coposer blobs.
+- Add functionality to click on them and see information.
+- Animate between stages.
+
+"""
+
